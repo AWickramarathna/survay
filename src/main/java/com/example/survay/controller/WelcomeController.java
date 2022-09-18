35e95352c9eef6b1c967429b0318cbebb30555cb
AWickramarathna <awickramarathna@typefi.com>
2022-09-18 17:10:50 +0530

    added view user and view attribute functions

--- src/main/java/com/example/survay/controller/WelcomeController.java
+++ src/main/java/com/example/survay/controller/WelcomeController.java
@@ -3,11 +3,15 @@ package com.example.survay.controller;
 import com.example.survay.model.Infor;
 import com.example.survay.model.SubmissionModel;
 import com.example.survay.Attributes;
+import java.util.List;
+
+import com.example.survay.repository.AttributeRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -29,9 +33,22 @@ class WelcomeController {
         return "description"; //view
     }
 
+    @GetMapping("/viewUser")
+    public String viewUserPage( Model model) {
+        model.addAttribute("allemplist", inforRepository.findAll());
+        return "viewUser";
+    }
+
+    @GetMapping("/viewResponse/{username}")
+    public String viewHomePage(@PathVariable(value = "username") String username, Model model) {
+        model.addAttribute("allemplist", attributeRepository.findAllByUsername(username));
+        return "viewAttribute";
+    }
+
     @PostMapping ("/infor")
     public String collectInfor(@ModelAttribute Infor infor,Model model) {
        inforRepository.save(infor);
+        model.addAttribute("allemplist", inforRepository.findAll());
         return "success";
     }
 
--- src/main/java/com/example/survay/repository/AttributeRepository.java
+++ src/main/java/com/example/survay/repository/AttributeRepository.java
@@ -1,7 +1,13 @@
 package com.example.survay.repository;
 
+import com.example.survay.Attributes;
+
+import java.util.List;
+
 @org.springframework.stereotype.Repository
 public interface AttributeRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.survay.Attributes, Long> {
 
     com.example.survay.Attributes findByUsernameAndOptionAndScenario(String user_key,String option,String s);
+
+    List<Attributes> findAllByUsername(String username);
 }
